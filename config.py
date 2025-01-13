8ac676b937e7adab0de25dd5ee7d4caa4ee9038f
Gilad Morad <55271904+gilad12-coder@users.noreply.github.com>
2025-01-13 23:54:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,23 @@
 import datetime
 
+# Date range settings
 start_date = datetime.date(2024, 1, 1)
 end_date = datetime.date(2024, 1, 3)
+
+# Data fetching settings
 daily_sample_size = 50
 vector_column = "EmbeddingVector"
+
+# Clustering settings
 clustering_method = "hdbscan"
 num_clusters = 5
-samples_per_cluster = 5
+total_daily_samples = 25
 use_pca = True
 pca_components = 10
 min_cluster_size = 5
 n_neighbors_faiss = 15
+
+# Azure Data Explorer settings
 cluster_url = "https://<YourClusterName>.<region>.kusto.windows.net"
 tenant_id = "<YourTenantID>"
 database = "<YourDatabase>"
